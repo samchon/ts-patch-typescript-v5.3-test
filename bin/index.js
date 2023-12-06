c20b9a0421269680c2d5b943a8b0ca5451ce9ccf
Jeongho Nam <samchon@wrtn.io>
2023-12-06 13:34:51 +0900

    Working well

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const typia_1 = __importDefault(require("typia"));
 console.log((input => {
-    return "object" === typeof input && null !== input && ("number" === typeof input.id && "string" === typeof input.name);
+    return "object" === typeof input && null !== input && ("number" === typeof input.id && (Math.floor(input.id) === input.id && 0 <= input.id && input.id <= 4294967295 && 1 <= input.id) && ("string" === typeof input.name && 2 <= input.name.length));
 }).toString(), JSON.stringify({
     schemas: [
         {
@@ -18,14 +18,80 @@ console.log((input => {
                 type: "object",
                 properties: {
                     id: {
+                        description: "Primary Key.",
+                        "x-typia-jsDocTags": [
+                            {
+                                name: "type",
+                                text: [
+                                    {
+                                        text: "uint",
+                                        kind: "text"
+                                    }
+                                ]
+                            },
+                            {
+                                name: "minimum",
+                                text: [
+                                    {
+                                        text: "1",
+                                        kind: "text"
+                                    }
+                                ]
+                            }
+                        ],
                         "x-typia-required": true,
                         "x-typia-optional": false,
-                        type: "number"
+                        type: "integer",
+                        minimum: 1,
+                        "x-typia-typeTags": [
+                            {
+                                target: "number",
+                                name: "Type<\"uint32\">",
+                                kind: "type",
+                                value: "uint32",
+                                validate: "Math.floor($input) === $input && 0 <= $input && $input <= 4294967295",
+                                exclusive: true
+                            },
+                            {
+                                target: "number",
+                                name: "Minimum<1>",
+                                kind: "minimum",
+                                value: 1,
+                                validate: "1 <= $input",
+                                exclusive: [
+                                    "minimum",
+                                    "exclusiveMinimum"
+                                ]
+                            }
+                        ]
                     },
                     name: {
+                        description: "Name of the member.",
+                        "x-typia-jsDocTags": [
+                            {
+                                name: "minLength",
+                                text: [
+                                    {
+                                        text: "2",
+                                        kind: "text"
+                                    }
+                                ]
+                            }
+                        ],
                         "x-typia-required": true,
                         "x-typia-optional": false,
-                        type: "string"
+                        type: "string",
+                        minLength: 2,
+                        "x-typia-typeTags": [
+                            {
+                                target: "string",
+                                name: "MinLength<2>",
+                                kind: "minLength",
+                                value: 2,
+                                validate: "2 <= $input.length",
+                                exclusive: true
+                            }
+                        ]
                     }
                 },
                 nullable: false,
